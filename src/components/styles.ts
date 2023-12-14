48866d97bba64072d6df20c341ec5321ddde669e
Diogo Evangelista <dhesousa77@gmail.com>
2023-12-14 12:56:51 -0300

    Cores & fontes

--- src/components/styles.ts
+++ src/components/styles.ts
@@ -6,23 +6,10 @@ interface ButtonContainerProps {
   variant: ButtonVariant;
 }
 
-const buttonVariantsColors = {
-  primary: "purple",
-  secondary: "orange",
-  danger: "red",
-  success: "green",
-};
-
 export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 40px;
 
-  background-color: ${(props) => props.theme.colors.primary};
-  color: ${({ theme }) => theme.colors.secondary};
-
-  /* ${(props) => {
-    return css`
-      background-color: ${buttonVariantsColors[props.variant]};
-    `;
-  }} */
+  background-color: ${(props) => props.theme.colors["green-300"]};
+  color: ${({ theme }) => theme.colors.white};
 `;
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -6,6 +6,17 @@ export const GlobalStyle = createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
   }
+
+  :focus {
+    outline: none;
+    box-shadow: 0 0 0 2px ${({ theme }) => theme.colors["green-500"]};
+  }
+
+  body, input, textarea, button {
+    font-family: 'Roboto', sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+  }
   body {
     background: #333;
     color: #FFF;
